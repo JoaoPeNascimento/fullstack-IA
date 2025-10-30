bddd7dbbbd4d99cb7d5d0cfc57030bec33bec1fa
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-10-30 16:17:09 -0300

    conteinerização dos serviços

--- frontend/src/api/summarize.js
+++ frontend/src/api/summarize.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api/v1";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1";
 
 export const summarizeText = async (inputText) => {
   try {
+    console.log(API_URL);
     const response = await axios.post(`${API_URL}/analyze`, {
       task: "summarize",
       input_text: inputText,
